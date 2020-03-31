65d90c4004322363e28aee2d81732d0b23d4a396
Victor Felder <victor@draft.li>
2020-03-31 13:36:52 +0200

    fix slug pipeline

--- database/get-dump.sh
+++ database/get-dump.sh
@@ -2,3 +2,4 @@
 wget https://cms.lobbywatch.ch/sites/lobbywatch.ch/files/exports/lobbywatch_export.sql.zip
 unzip lobbywatch_export.sql.zip
 rm lobbywatch_export.sql.zip
+sed -i.bak 's@smallint(6) unsigned@int(6)@' lobbywatch.sql
