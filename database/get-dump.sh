4ee807ccd367a221e0bcc6a04c4439e1790cb635
Victor Felder <victor@draft.li>
2020-04-06 12:51:07 +0200

    Make it easier to inspect local sql data

--- database/get-dump.sh
+++ database/get-dump.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+BEFORE=$(wc -l lobbywatch.sql)
 wget https://cms.lobbywatch.ch/sites/lobbywatch.ch/files/exports/lobbywatch_export.sql.zip
 unzip lobbywatch_export.sql.zip
 rm lobbywatch_export.sql.zip
+AFTER=$(wc -l lobbywatch.sql)
+echo "wc -l lobbywatch.sql BEFORE: $BEFORE"
+echo "wc -l lobbywatch.sql AFTER:  $AFTER"
 sed -i.bak 's@smallint(6) unsigned@int(6)@' lobbywatch.sql
