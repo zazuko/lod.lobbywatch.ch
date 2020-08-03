3ccd4b13939aaf0fb5af563ae609884032f55a75
Victor Felder <victor@draft.li>
2020-08-03 17:05:44 +0200

    update ontop to 4.0.0

--- ontop-scripts/1-convert.sh
+++ ontop-scripts/1-convert.sh
@@ -9,8 +9,8 @@ else
   # install ontop
   mkdir ontop
   cd ontop
-  wget https://github.com/ontop/ontop/releases/download/ontop-4.0.0-rc-1/ontop-cli-4.0.0-rc-1.zip
-  unzip ontop-cli-4.0.0-rc-1.zip
+  wget https://github.com/ontop/ontop/releases/download/ontop-4.0.0/ontop-cli-4.0.0.zip
+  unzip ontop-cli-4.0.0.zip
   cd ..
   # install mysql driver
   cp mysql-connector-java-5.1.49/mysql-connector-java-5.1.49.jar ontop/jdbc/
