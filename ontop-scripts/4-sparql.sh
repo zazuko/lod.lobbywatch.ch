1063f7577dd4666a5133291adbf22e41f2fd589c
magdalena@surowka.ch <magdalena@surowka.ch>
2021-12-02 16:24:22 +0100

    access rights

--- ontop-scripts/4-sparql.sh
+++ ontop-scripts/4-sparql.sh
@@ -53,3 +53,10 @@ curl \
     --header "Content-Type: application/sparql-update" \
     --data-binary '@queries/memberships.rq' \
     'http://data.zazuko.com/lobbywatch/update'
+
+curl \
+    --include -v \
+    --header "Authorization: Basic $STARDOG_AUTH" \
+    --header "Content-Type: application/sparql-update" \
+    --data-binary '@queries/access_right.rq' \
+    'http://data.zazuko.com/lobbywatch/update'
